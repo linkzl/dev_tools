509d4ee65568105844ac07fe4c69777bee93b9ad
Louis BOURY <linkzl.linkzl@gmail.com>
2017-04-12 18:00:21 +0200

    retour à la normale

--- ProjectMaven/lboury/src/main/java/lboury/App.java
+++ ProjectMaven/lboury/src/main/java/lboury/App.java
@@ -8,8 +8,8 @@ public class App
 {
 	public int max(int a, int b)
     {
-    	//return a>b?a:b;
-		return a;//le stagiaire
+    	return a>b?a:b;
+		//return a;//le stagiaire
     }
 	
 	public static void main( String[] args )
@@ -17,6 +17,12 @@ public class App
         System.out.println( "Bonjour Monde !" );
         App monApp = new App();
         System.out.println(monApp.max(4,5));
+        
+        int monmax=0;
+        CSVReader reader = new CSVReader(new FileReader("data.csv"));
+        List<String[]> myEntries = reader.readAll();
+        
+        
     }
     
     
